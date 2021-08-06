9386c9a295b5463a0325795618168cbbb1ff166f
Milo Cress <cress@mit.edu>
2021-08-06 12:02:02 -0400

    completed challenge 2

--- packages/hardhat/contracts/Vendor.sol
+++ packages/hardhat/contracts/Vendor.sol
@@ -1,19 +1,34 @@
 pragma solidity >=0.6.0 <0.7.0;
 
-//import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 import "./YourToken.sol";
 
-contract Vendor {
+contract Vendor is Ownable {
 
   YourToken yourToken;
+  uint256 public constant tokensPerEth = 100;
+
+  event BuyTokens(address, uint256, uint256);
+  event SellTokens(address, uint256, uint256);
 
   constructor(address tokenAddress) public {
     yourToken = YourToken(tokenAddress);
   }
 
   //ToDo: create a payable buyTokens() function:
+  function buyTokens() public payable {
+    uint256 tokensAmount = msg.value * tokensPerEth;
+    yourToken.transfer(msg.sender, tokensAmount);
+    emit BuyTokens(msg.sender, msg.value, tokensAmount);
+  }
 
   //ToDo: create a sellTokens() function:
+  function sellTokens(uint256 numTokens) public {
+    uint256 ethAmount = numTokens / tokensPerEth;
+    yourToken.transferFrom(msg.sender, address(this), numTokens);
+    msg.sender.transfer(ethAmount);
+    emit SellTokens(msg.sender, ethAmount, numTokens);
+  }
 
   //ToDo: create a withdraw() function that lets the owner, you can 
   //use the Ownable.sol import above:
--- packages/hardhat/scripts/deploy.js
+++ packages/hardhat/scripts/deploy.js
@@ -12,17 +12,19 @@ const main = async () => {
   const yourToken = await deploy("YourToken")
 
   //Todo: deploy the vendor
-  //const vendor = await deploy("Vendor",[ yourToken.address ])
+  const vendor = await deploy("Vendor",[ yourToken.address ])
 
   //console.log("\n 🏵  Sending all 1000 tokens to the vendor...\n");
   //Todo: transfer the tokens to the vendor
-  //const result = await yourToken.transfer( vendor.address, utils.parseEther("1000") );
+  const result = await yourToken.transfer( vendor.address, utils.parseEther("1000") );
+
+  // const result = await yourToken.transfer("0x091fcA527b360bD732d943051ac171b3640098e2", utils.parseEther("1000") );
 
   //const stakerContract = await deploy("Staker",[ exampleExternalContract.address ]) // <-- add in constructor args like line 14 ^^^
 
   //console.log("\n 🤹  Sending ownership to frontend address...\n")
   //ToDo: change address with your burner wallet address vvvv
-  //await vendor.transferOwnership( "0xD75b0609ed51307E13bae0F9394b5f63A7f8b6A1" );
+  await vendor.transferOwnership( "0x091fcA527b360bD732d943051ac171b3640098e2" );
 
   //const secondContract = await deploy("SecondContract")
 
